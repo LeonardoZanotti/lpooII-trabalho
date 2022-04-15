dda7d18473874d605143818e5674848aac3c4988
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-04-15 18:48:21 -0300

    fixed database structure

--- lpooII_work/src/lpooii_work/database/migrate.sql
+++ lpooII_work/src/lpooii_work/database/migrate.sql
@@ -13,6 +13,15 @@ CREATE DATABASE lpooii;
 
 \c lpooii;
 
+CREATE TABLE tb_cliente (
+    id_cliente serial PRIMARY KEY,
+    nome_cliente VARCHAR(50) NOT NULL,
+    sobrenome_cliente VARCHAR(50) NOT NULL,
+    rg_cliente VARCHAR(15) NOT NULL,
+    cpf_cliente VARCHAR(15) NOT NULL,
+    endereco_cliente VARCHAR(100)
+);
+
 CREATE TABLE tb_conta (
     id_conta serial PRIMARY KEY,
     saldo_conta DOUBLE PRECISION,
@@ -20,31 +29,22 @@ CREATE TABLE tb_conta (
     deposito_minimo_conta DOUBLE PRECISION,
     limite_conta DOUBLE PRECISION,
     montante_minimo_conta DOUBLE PRECISION,
-    tipo_conta SMALLINT NOT NULL
+    tipo_conta SMALLINT NOT NULL,
+    id_cliente INTEGER,
+    CONSTRAINT fk_cliente FOREIGN KEY(id_cliente) REFERENCES tb_cliente(id_cliente) ON DELETE CASCADE
 );
 
-CREATE TABLE tb_cliente (
-    id_cliente serial PRIMARY KEY,
-    nome_cliente VARCHAR(50) NOT NULL,
-    sobrenome_cliente VARCHAR(50) NOT NULL,
-    rg_cliente VARCHAR(15) NOT NULL,
-    cpf_cliente VARCHAR(15) NOT NULL,
-    endereco_cliente VARCHAR(100),
-    id_conta INT,
-    CONSTRAINT fk_conta FOREIGN KEY(id_conta) REFERENCES tb_conta(id_conta)
-);
+INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente, endereco_cliente)
+VALUES ('cliente 1', 'sobrenome 1', '418757896', '56989846380', 'endereco 1');
 
-INSERT INTO tb_conta(deposito_inicial_conta, limite_conta, tipo_conta)
-VALUES (150.0, 2500.0, 1);
+INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente, endereco_cliente)
+VALUES ('cliente 2', 'sobrenome 2', '911225341', '31436693144', 'endereco 2');
 
-INSERT INTO tb_conta(saldo_conta, deposito_minimo_conta, montante_minimo_conta, tipo_conta)
-VALUES (655.25, 10.0, 300.0, 2);
+INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente, endereco_cliente)
+VALUES ('cliente 3', 'sobrenome 3', '418757896', '41303763494', 'endereco 3');
 
-INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente, endereco_cliente, id_conta)
-VALUES ('cliente 1', 'sobrenome 1', '418757896', '56989846380', 'endereco 1', 1);
+INSERT INTO tb_conta(deposito_inicial_conta, limite_conta, tipo_conta, id_cliente)
+VALUES (150.0, 2500.0, 1, 1);
 
-INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente, endereco_cliente, id_conta)
-VALUES ('cliente 2', 'sobrenome 2', '911225341', '31436693144', 'endereco 2', 2);
-
-INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente, endereco_cliente, id_conta)
-VALUES ('cliente 3', 'sobrenome 3', '418757896', '41303763494', 'endereco 3', null);
+INSERT INTO tb_conta(saldo_conta, deposito_minimo_conta, montante_minimo_conta, tipo_conta, id_cliente)
+VALUES (655.25, 10.0, 300.0, 2, 2);
