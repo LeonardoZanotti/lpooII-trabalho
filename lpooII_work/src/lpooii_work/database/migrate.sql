eeae83d1749180db61fcc678ec084020b819f4c3
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-04-15 19:30:32 -0300

    updated migration again

--- lpooII_work/src/lpooii_work/database/migrate.sql
+++ lpooII_work/src/lpooii_work/database/migrate.sql
@@ -41,10 +41,13 @@ INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente,
 VALUES ('cliente 2', 'sobrenome 2', '911225341', '31436693144', 'endereco 2');
 
 INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente, endereco_cliente)
-VALUES ('cliente 3', 'sobrenome 3', '418757896', '41303763494', 'endereco 3');
+VALUES ('cliente 3', 'sobrenome 3', '429434121', '41303763494', 'endereco 3');
 
-INSERT INTO tb_conta(deposito_inicial_conta, limite_conta, tipo_conta, id_cliente)
-VALUES (150.0, 2500.0, 1, 1);
+INSERT INTO tb_cliente(nome_cliente, sobrenome_cliente, rg_cliente, cpf_cliente, endereco_cliente)
+VALUES ('cliente 4', 'sobrenome 4', '403289440', '96455625706', 'endereco 4');
+
+INSERT INTO tb_conta(saldo_conta, deposito_inicial_conta, limite_conta, tipo_conta, id_cliente)
+VALUES (1583.13, 150.0, 2500.0, 1, 1);
 
 INSERT INTO tb_conta(saldo_conta, deposito_minimo_conta, montante_minimo_conta, tipo_conta, id_cliente)
 VALUES (655.25, 10.0, 300.0, 2, 2);
